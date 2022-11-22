50a4763a25a6ee1d62e24233e017e728a92e6689
Vladislavs <Vladikuk948@gmail.com>
2022-11-22 11:06:09 +0200

    blog list update

--- resources/views/blog-list.blade.php
+++ resources/views/blog-list.blade.php
@@ -1,76 +1,6 @@
-<!DOCTYPE html>
-<html lang="zxx">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="Palun, Personal Portfolio, One page portfolio, HTML Template, CSS3, HTML5, Website, Portfolio template">
-
-    <!-- title -->
-    <title>Palun - Blog List Page</title>
-
-    <!-- favicon -->
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
-    <!-- stylesheets -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/slick.min.css">
-    <link rel="stylesheet" href="assets/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
-    <link rel="stylesheet" href="assets/css/mean-menu.css">
-    <link rel="stylesheet" href="assets/css/default.css">
-    <link rel="stylesheet" href="style.css">
-
-</head>
-
-<body>
-    <!-- header area -->
-    <div class="header-area">
-        <div class="container mt-60 pl-0 pr-0">
-            <div class="row justify-content-lg-between">
-                <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 pl-xl-0">
-                    <!-- logo -->
-                    <div class="logo-area">
-                        <a href="/index.html">
-                            <img src="assets/img/logo/logo.png" alt="Logo" />
-                        </a>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-7 col-md-10 col-sm-8 col-8 pr-0">
-                    <div class="menu-area menu-social float-right">
-                        <ul class="list-inline pl-15">
-                            <li class="list-inline-item menu-border"><a href="#"><i class="fab fa-skype"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-                        </ul>
-                    </div>
-                    <nav id="mobile-nav">
-                        <div class="menu-area float-right">
-                            <ul class="list-inline">
-                                <li class="list-inline-item"><a href="index.blade.php">Home</a></li>
-                                <li class="list-inline-item"><a href="index.blade.php#portfolio">Portfolio</a>
-                                    <ul class="sub-menu pb-10 pt-10 pl-15 pr-15">
-                                        <li><a href="portfolio-details.blade.php">Portfolio Details</a></li>
-                                    </ul>
-                                </li>
-                                <li class="list-inline-item"><a href="index.blade.php#blog">Blog</a>
-                                    <ul class="sub-menu pb-10 pt-10 pl-15 pr-15">
-                                        <li><a href="blog-list.html">Blog List</a></li>
-                                        <li><a href="news-details.blade.php">News Details</a></li>
-                                    </ul>
-                                </li>
-                                <li class="list-inline-item"><a href="index.blade.php#contact">Contact</a></li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <div class="mobile-menu"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- header area ends -->
-
+@section('header')
     <!-- page title area starts -->
     <div class="page-title-area">
         <div class="container">
@@ -86,7 +16,9 @@
         </div>
     </div>
     <!-- page title area end -->
+@endsection
 
+@section('content')
     <!-- blog post section starts -->
     <div class="blog-post-area bgs pt-110 pb-120">
         <div class="container">
@@ -101,7 +33,7 @@
                         <div class="row">
                             <div class="col-xl-5 col-lg-5">
                                 <div class="list-blog-img">
-                                    <img src="assets/img/blog/list-1.jpg" alt="list 1">
+                                    <img src="http://localhost/img/blog/list-1.jpg" alt="list 1">
                                 </div>
                                 <div class="list-blog-meta">
                                     <div class="date">
@@ -143,7 +75,7 @@
                         <div class="row">
                             <div class="col-xl-5 col-lg-5">
                                 <div class="list-blog-img">
-                                    <img src="assets/img/blog/list-2.jpg" alt="list 1">
+                                    <img src="http://localhost/img/blog/list-2.jpg" alt="list 1">
                                 </div>
                                 <div class="list-blog-meta">
                                     <div class="date">
@@ -193,7 +125,7 @@
                         <div class="row">
                             <div class="col-xl-5 col-lg-5">
                                 <div class="list-blog-img">
-                                    <img src="assets/img/blog/list-3.jpg" alt="list 1">
+                                    <img src="http://localhost/img/blog/list-3.jpg" alt="list 1">
                                 </div>
                                 <div class="list-blog-meta">
                                     <div class="date">
@@ -235,7 +167,7 @@
                         <div class="row">
                             <div class="col-xl-5 col-lg-5">
                                 <div class="list-blog-img">
-                                    <img src="assets/img/blog/list-4.jpg" alt="list 1">
+                                    <img src="http://localhost/img/blog/list-4.jpg" alt="list 1">
                                 </div>
                                 <div class="list-blog-meta">
                                     <div class="date">
@@ -286,7 +218,7 @@
                         <div class="row">
                             <div class="col-xl-5 col-lg-5">
                                 <div class="list-blog-img">
-                                    <img src="assets/img/blog/list-5.jpg" alt="list 1">
+                                    <img src="http://localhost/img/blog/list-5.jpg" alt="list 1">
                                 </div>
                                 <div class="list-blog-meta">
                                     <div class="date">
@@ -324,7 +256,7 @@
                         <div class="row">
                             <div class="col-xl-5 col-lg-5">
                                 <div class="list-blog-img">
-                                    <img src="assets/img/blog/list-6.jpg" alt="list 1">
+                                    <img src="http://localhost/img/blog/list-6.jpg" alt="list 1">
                                 </div>
                                 <div class="list-blog-meta">
                                     <div class="date">
@@ -396,32 +328,5 @@
         </div>
     </div>
     <!-- newsletter area ends -->
+@endsection
 
-    <!-- footer copyright area starts -->
-    <div class="footer-area">
-        <div class="copyright-text text-center">
-            <p class="pt-20"><i class="fas fa-copyright mr-10"></i>Copyright - HJ | All Rights Rerved</p>
-        </div>
-
-        <div class="back-to-top">
-            <a href="#"><i class="fas fa-angle-up"></i></a>
-        </div>
-    </div>
-    <!-- footer copyright area end -->
-
-
-    <!-- scripts -->
-    <script src="assets/js/jquery-2.2.4.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/slick.js"></script>
-    <script src="assets/js/jquery-1.12.4.js"></script>
-    <script src="assets/js/waypoint.min.js"></script>
-    <script src="assets/js/countup.js"></script>
-    <script src="assets/js/isotope.min.js"></script>
-    <script src="assets/js/jquery-mean-menu.min.js"></script>
-    <script src="assets/js/magnific-popup.js"></script>
-    <script src="assets/js/script.js"></script>
-</body>
-
-</html>
