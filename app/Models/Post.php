d5902cba7b24f0a412e8ddfdb2faa30ec2e954ba
Vladislavs <Vladikuk948@gmail.com>
2022-11-23 10:34:46 +0200

    Models relationships update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+
+    public function Tags()
+    {
+        return $this->hasMany(Tag::Class);
+    }
+
+    public function Category()
+    {
+        return $this->hasOne(Category::Class);
+    }
 }
