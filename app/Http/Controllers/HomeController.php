ba078f31789cae85323c7c6c85889db1c330ef1f
Vladislavs <Vladikuk948@gmail.com>
2022-11-23 11:51:53 +0200

    Homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+
+
+    public function index()
+    {
+        $post = Post::where('is_published', true);
+
+
+
+        return view('index', [
+            'post' => $post
+        ]);
+    }
+
+
 }
