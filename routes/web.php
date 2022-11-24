33022236bc2703ad2f8bdae812315fe8f0ba6cd6
Vladislavs <Vladikuk948@gmail.com>
2022-11-24 11:28:19 +0200

    loign breezee web server and so one

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
 Route::get('/portfolio', function () {
@@ -32,8 +32,15 @@ Route::get('/news', function () {
 });
 
 
-Route::get('/test', function () {
-    return view('test', ['name' => 'samantha']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
+require __DIR__.'/auth.php';
